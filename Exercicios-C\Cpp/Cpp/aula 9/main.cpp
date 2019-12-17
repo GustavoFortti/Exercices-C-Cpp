b47759276a8e90fa4eaf47517d53913a07557af0
GustavoFortti <53984056+GustavoFortti@users.noreply.github.com>
2019-12-16 23:39:03 -0300

    Update main.cpp

--- Exercicios-C\Cpp/Cpp/aula 9/main.cpp
+++ Exercicios-C\Cpp/Cpp/aula 9/main.cpp
@@ -8,30 +8,37 @@ public:
     int id;
     double balance;
 
-    double deposit( double amount)
-    {
-        if( amount > 0)
-        {
-            balance += amount;
-        }
-        return balance;
-    }
-    double withdraw( double amount)
-    {
-        if( amount <= balance)
-        {
-            balance -= amount;
-        }
-        return balance;
-    }
+    double deposit( double amount);
+    double withdraw( double amount);
 };
+
+double Acount::deposit( double amount)
+{
+    if( amount > 0)
+    {
+        balance += amount;
+    }
+    return balance;
+}
+
+double Acount::withdraw( double amount)
+{
+    if( amount <= balance)
+    {
+        balance -= amount;
+    }
+    return balance;
+}
 
 int main()
 {
     Acount c;
+    Acount* pc = &c;
 
-    c.id = 1;
-    c.balance = 100.75;
+    (*pc).id = 1;
+    (*pc).balance = 100.75;
+
+
 
     c.deposit(100);
     cout << c.balance << endl;
